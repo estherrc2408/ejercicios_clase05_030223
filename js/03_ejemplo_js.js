b4c6777807207e0a5dc11bc72cec143be8ddf28e
unknown <badger24a@gmail.com>
2023-02-03 19:02:49 +0100

    ejercicios y ejemplos del primer dia con javascript

---
+++ js/03_ejemplo_js.js
@@ -0,0 +1,2 @@
+/*Este script nos dará un mensaje de alerta que saltará nada más abrir la página en eel cual ponga hoola*/
+alert('hoola');
---
+++ js/04_ejemplo_js.js
@@ -0,0 +1,5 @@
+/*La funcion saludo() podrá ser llamada por el documento HTML enlazado,
+esta funcion hace saltar un mensaje de alerta al usuario ey mundo*/
+function saludo(){
+    alert('ey Mundo');
+}
